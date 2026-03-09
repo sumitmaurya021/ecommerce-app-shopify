4e32858a9afd44bde5ef3b11c62ad10dc5101df1
Sumit Maurya <mauryasumit222@gmail.com>
2026-03-09 18:56:58 +0530

    Completing Orders Fetch Functionality

--- web/frontend/components/Card.jsx
+++ web/frontend/components/Card.jsx
@@ -1,11 +1,18 @@
 import { Layout, LegacyCard } from '@shopify/polaris'
 import React from 'react'
 
-export function Card({title}) {
+export function Card({title, data, productCard, collectionCard, orderCard, fulfillCard, remainsCard}) {
   return (
     <>
         <Layout.Section oneThird>
             <LegacyCard title={title} sectioned>
+              <h1 className='total_count'>
+                {productCard && data}
+                {collectionCard && data}
+                {orderCard && data}
+                {fulfillCard && data}
+                {remainsCard && data}
+              </h1>
             </LegacyCard>
         </Layout.Section>
     </>
--- web/frontend/components/TopBar.jsx
+++ web/frontend/components/TopBar.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import logo from '../assets/Images/logo.jpg'
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/Images/logo.jpg";
 
 export function TopBar() {
+  const [ name, setName ] = React.useState("");
+
+  useEffect(() => {
+
+    const getStoreInfo = async () => {
+      try {
+        const res = await fetch("/api/store/info", {
+          method: "GET",
+        });
+
+        const storeData = await res.json();
+        setName(storeData.data[0].name);
+        console.log("store data:", storeData);
+
+      } catch (error) {
+        console.error("error:", error);
+      }
+    };
+
+    getStoreInfo();
+
+  });
+
   return (
-    <div className='topbar-section'>
-        <div className="logo-block">
-            <img className='logo' src={logo} alt="logo image" />
-            <h1 className='text-bold h4'>Shop Dashboard</h1>
-            <NavLink to="/"> Sales </NavLink>
-            <NavLink to="/products"> Products </NavLink>
-        </div>
+    <div className="topbar-section">
+      <div className="logo-block">
+        <img className="logo" src={"https://images-platform.99static.com//y0rb96b9CUsj6F8lqnkVOPlBuyY=/0x0:999x999/fit-in/500x500/99designs-contests-attachments/109/109048/attachment_109048124"} alt="logo image" />
+        <h1 className="text-bold h4">{name}</h1>
+        <NavLink to="/">Sales</NavLink>
+        <NavLink to="/products">Products</NavLink>
+      </div>
     </div>
-  )
+  );
 }
